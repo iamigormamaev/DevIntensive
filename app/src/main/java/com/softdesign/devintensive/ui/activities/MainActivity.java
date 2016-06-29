974a159123beef738ff621a07f38c6d68a4bd190
Igor Mamaev <mraction@mail.ru>
2016-06-29 03:03:37 +0300

    Реализовал Navigation Drawer согласно макету. Реализовал обработку нажатия системной кнопки back.  Реализовал Collapsing Toolbar.

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,27 +1,102 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.graphics.Color;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.CoordinatorLayout;
 import android.os.Bundle;
-import android.support.v7.view.ActionMode;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.data.managers.DataManager;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
     private static final String TAG = ConstantManager.TAG_PREFIX + "MainActivity";
+
+
+    private int mCurrentEditMode = 0;
+
+    private ImageView mCallImg;
+    private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolbar;
+    private DrawerLayout mNavigationDraver;
+    private FloatingActionButton mFloatingActionButton;
+    private EditText mUserPhone, mUserMail, mUserVk, mUserGit, mUserBio;
+    private List<EditText> mUserInfoViews;
+    private DataManager mDataManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate");
+        mCallImg = (ImageView) findViewById(R.id.account_mobile_phone_call_img);
+        mCallImg.setOnClickListener(this);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_layout);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mNavigationDraver = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mFloatingActionButton = (FloatingActionButton) findViewById(R.id.floating_action_button);
+        mFloatingActionButton.setOnClickListener(this);
+        mUserPhone = (EditText) findViewById(R.id.account_mobile_phone_ev);
+        mUserMail = (EditText) findViewById(R.id.account_email_ev);
+        mUserVk = (EditText) findViewById(R.id.account_vk_ev);
+        mUserGit = (EditText) findViewById(R.id.account_repository_ev);
+        mUserBio = (EditText) findViewById(R.id.account_about_me_ev);
+        mDataManager = DataManager.getInstance();
 
+        mUserInfoViews = new ArrayList<>();
+        mUserInfoViews.add(mUserPhone);
+        mUserInfoViews.add(mUserMail);
+        mUserInfoViews.add(mUserVk);
+        mUserInfoViews.add(mUserGit);
+        mUserInfoViews.add(mUserBio);
+        Log.d(TAG, "onCreate");
+        setupToolbar();
+        setupDrawer();
+        loadUserInfoValue();
+
+        if (savedInstanceState == null) {
+
+        } else {
+            mCurrentEditMode = savedInstanceState.getInt(ConstantManager.EDIT_MODE_KEY, 0);
+            changeEditMode(mCurrentEditMode);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(ConstantManager.EDIT_MODE_KEY, mCurrentEditMode);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mNavigationDraver.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (mNavigationDraver.isDrawerOpen(GravityCompat.START)) {
+            mNavigationDraver.closeDrawer(GravityCompat.START);
+        } else super.onBackPressed();
     }
 
     @Override
@@ -54,10 +129,94 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onDestroy");
     }
 
+
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d(TAG, "onRestart");
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.account_mobile_phone_call_img:
+                break;
+            case R.id.floating_action_button:
+
+
+                if (mCurrentEditMode == 0) {
+                    mCurrentEditMode = 1;
+                    changeEditMode(mCurrentEditMode);
+                } else {
+                    mCurrentEditMode = 0;
+                    changeEditMode(mCurrentEditMode);
+                }
+                break;
+        }
+    }
+
+    private void showSnackbar(String message) {
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupDrawer() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                showSnackbar(item.getTitle().toString());
+                item.setChecked(true);
+                mNavigationDraver.closeDrawer(GravityCompat.START);
+                return false;
+
+            }
+        });
+
+    }
+
+    private void changeEditMode(int mode) {
+        if (mode == 1) {
+            for (EditText userValue : mUserInfoViews) {
+                userValue.setEnabled(true);
+                userValue.setFocusable(true);
+                userValue.setFocusableInTouchMode(true);
+                mFloatingActionButton.setImageResource(R.drawable.ic_done_black_24dp);
+            }
+        } else {
+            for (EditText userValue : mUserInfoViews) {
+                saveUserInfoValue();
+                userValue.setEnabled(false);
+                userValue.setFocusable(false);
+                userValue.setFocusableInTouchMode(false);
+                mFloatingActionButton.setImageResource(R.drawable.ic_create_black_24dp);
+            }
+        }
+
+    }
+
+    private void loadUserInfoValue() {
+        List<String> userData = mDataManager.getPreferenceManager().loadUserProfileData();
+        for (int i = 0; i < userData.size(); i++) {
+            mUserInfoViews.get(i).setText(userData.get(i));
+        }
+    }
+
+
+    private void saveUserInfoValue() {
+        List<String> userData = new ArrayList<>();
+        for (EditText userFieldView :
+                mUserInfoViews) {
+            userData.add(userFieldView.getText().toString());
+        }
+        mDataManager.getPreferenceManager().saveUserProfileData(userData);
+    }
 }
